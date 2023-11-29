8eac53b8dcff64d18136a21e5bdb209835e389d5
RomiRao <raoromina96@gmail.com>
2023-11-29 16:56:03 -0300

    formcontrol added to stylize inputs

--- src/components/forms/submitform.jsx
+++ src/components/forms/submitform.jsx
@@ -1,9 +1,15 @@
 //import React from 'react'
 import { useState } from "react";
 
-import { Box, TextField, Select, MenuItem } from "@mui/material";
+import { Box, TextField, Select, MenuItem, FormControl, InputLabel } from "@mui/material";
 
 export default function Submitform() {
+
+  const [input, setInput] = useState({
+    task: "",
+    category: "",
+    complete: false,
+  });
 
   const options = [
     {value: 'Not Important', text: 'Not Important'},
@@ -15,17 +21,20 @@ export default function Submitform() {
   const [selected, setSelected] = useState(options[0].value);
 
   const handleChange = e => {
-    console.log(e.target.value);
     setSelected(e.target.value);
   };
 
   return (
     <Box component='form'>
+    <FormControl  sx={{ m: 1, minWidth: 120 }} size="small">
     <TextField
           label="Enter your task"
           id="outlined-size-small"
           size="small"
         />
+    </FormControl>
+<FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Category</InputLabel>
         <Select
           labelId="category"
           id="category"
@@ -37,7 +46,7 @@ export default function Submitform() {
             <MenuItem key={option.value} value={option.value}>{option.text}</MenuItem>
           ))}
         </Select>
-
+</FormControl>
     </Box>
   )
 }
