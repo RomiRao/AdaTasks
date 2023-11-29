b5e6d7134e934d35949609aa13165fcc319f671f
RomiRao <raoromina96@gmail.com>
2023-11-29 15:58:28 -0300

    select with handlechange done

--- src/components/forms/submitform.jsx
+++ src/components/forms/submitform.jsx
@@ -1,7 +1,43 @@
 //import React from 'react'
+import { useState } from "react";
+
+import { Box, TextField, Select, MenuItem } from "@mui/material";
 
 export default function Submitform() {
+
+  const options = [
+    {value: 'Not Important', text: 'Not Important'},
+    {value: 'Important', text: 'Important'},
+    {value: 'Urgent', text: 'Urgent'},
+  ];
+
+
+  const [selected, setSelected] = useState(options[0].value);
+
+  const handleChange = e => {
+    console.log(e.target.value);
+    setSelected(e.target.value);
+  };
+
   return (
-    <div>submitform</div>
+    <Box component='form'>
+    <TextField
+          label="Enter your task"
+          id="outlined-size-small"
+          size="small"
+        />
+        <Select
+          labelId="category"
+          id="category"
+          value={selected}
+          label="Category"
+          onChange={handleChange}
+        >
+          {options.map(option => (
+            <MenuItem key={option.value} value={option.value}>{option.text}</MenuItem>
+          ))}
+        </Select>
+
+    </Box>
   )
 }
