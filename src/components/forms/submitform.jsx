d22843310ed4807ced8d1fab861e8be7b69c99ef
RomiRao <raoromina96@gmail.com>
2023-11-29 18:02:50 -0300

    formik validation added, still figuring out how to do it properly with select

--- src/components/forms/submitform.jsx
+++ src/components/forms/submitform.jsx
@@ -1,36 +1,51 @@
-//import React from 'react'
-import { useState } from "react";
-
+//import { useState } from "react";
 import { Box, TextField, Select, MenuItem, FormControl, InputLabel } from "@mui/material";
+import { useFormik } from 'formik';
 
 export default function Submitform() {
+  const formik = useFormik({
+    initialValues: {
+      task: "",
+      category: "",
+      complete: false,
+    },
+    validate: values => {
+        const errors = {};
+        if(!values.task) {
+            errors.task = 'Required';
+        } else if (values.task.length < 4 || values.task.length > 15) {
+            errors.task = "Must be between 4 and 15 characters"
+        }
+        return errors
+    },
+    onSubmit: values => {
+        alert(JSON.stringify(values, null, 2));
+    },
 
-  const [input, setInput] = useState({
-    task: "",
-    category: "",
-    complete: false,
-  });
-
-  const options = [
-    {value: 'Not Important', text: 'Not Important'},
-    {value: 'Important', text: 'Important'},
-    {value: 'Urgent', text: 'Urgent'},
-  ];
+    
+});
 
 
-  const [selected, setSelected] = useState(options[0].value);
+  const options = ['Not Important' , 'Important' , 'Urgent'];
 
-  const handleChange = e => {
-    setSelected(e.target.value);
-  };
+
+  // const [selected, setSelected] = useState(options[0].value);
+
+  // const handleChange = e => {
+  //   setSelected(e.target.value);
+  // };
 
   return (
-    <Box component='form'>
+    <Box component='form' onSubmit={formik.handleSubmit} p={3}>
     <FormControl  sx={{ m: 1, minWidth: 120 }} size="small">
     <TextField
+          error={formik.errors.task ? true : false}
           label="Enter your task"
-          id="outlined-size-small"
+          id="task"
           size="small"
+          onChange={formik.handleChange}
+          value={formik.values.task} name="task" 
+          helperText={formik.errors.task ? formik.errors.task : null}
         />
     </FormControl>
 <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
@@ -38,12 +53,12 @@ export default function Submitform() {
         <Select
           labelId="category"
           id="category"
-          value={selected}
+          value={formik.category}
           label="Category"
-          onChange={handleChange}
+          onChange={formik.handleChange}
         >
           {options.map(option => (
-            <MenuItem key={option.value} value={option.value}>{option.text}</MenuItem>
+            <MenuItem key={option} value={option}>{option}</MenuItem>
           ))}
         </Select>
 </FormControl>
