9826af792b9134218bb44b0b2ecfcf5becc770ca
RomiRao <raoromina96@gmail.com>
2023-11-29 19:12:58 -0300

    defining showform structure

--- src/components/forms/showform.jsx
+++ src/components/forms/showform.jsx
@@ -1,7 +1,43 @@
-//import React from 'react'
+
+import { InputLabel, MenuItem, Select, Typography, FormControl } from "@mui/material"
 
 export default function Showform() {
+
+  const options = ['Not Important' , 'Important' , 'Urgent'];
+
   return (
-    <div>showform</div>
+    <>
+      <Typography variant="h5">
+        Filter by
+      </Typography>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Category</InputLabel>
+        <Select
+          labelId="state"
+          id="state"
+          //value={formik.category}
+          label="State"
+          //onChange={formik.handleChange}
+        >
+          <MenuItem>All</MenuItem>
+          <MenuItem>Complete</MenuItem>
+          <MenuItem>Uncomplete</MenuItem>
+        </Select>
+</FormControl>
+<FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Category</InputLabel>
+        <Select
+          labelId="category"
+          id="category"
+          //value={formik.category}
+          label="Category"
+          //onChange={formik.handleChange}
+        >
+          {options.map(option => (
+            <MenuItem key={option} value={option}>{option}</MenuItem>
+          ))}
+        </Select>
+</FormControl>
+    </>
   )
 }
