ee88bfab8384a4536a1a5b8f6512b4fe03be4c39
RomiRao <raoromina96@gmail.com>
2023-11-29 15:35:42 -0300

    general forms structure done

--- src/components/forms/formsContainer.jsx
+++ src/components/forms/formsContainer.jsx
@@ -3,10 +3,12 @@
 import Submitform from "./submitform";
 import Showform from "./Showform"
 import { Box } from "@mui/material";
+import image from "../../../public/images/todo-title.png"
 
 export default function FormsContainer() {
   return (
-    <Box>
+    <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', p: '50px'}}>
+      <img style={{maxWidth: '450px', width: '90%'}} src={image} alt="title todo ilustration" />
       <Submitform />
       <Showform />
     </Box>
