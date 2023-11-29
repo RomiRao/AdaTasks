70724ba0499d8ca50e8f233a6398ce8d62a0cee6
RomiRao <raoromina96@gmail.com>
2023-11-29 14:45:08 -0300

    creating task components

---
+++ src/components/task-list/tasksContainer.jsx
@@ -0,0 +1,7 @@
+//import React from 'react'
+
+export default function tasksContainer() {
+  return (
+    <div>tasksContainer</div>
+  )
+}
---
+++ src/components/task-list/tasksInfo.jsx
@@ -0,0 +1,7 @@
+//import React from 'react'
+
+export default function tasksInfo() {
+  return (
+    <div>tasksInfo</div>
+  )
+}
