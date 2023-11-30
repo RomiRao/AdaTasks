bc22330c2879d171dda5c99d286b7c62e841bc35
RomiRao <raoromina96@gmail.com>
2023-11-30 12:49:07 -0300

    changin images from directory because react wants it c:

--- src/components/task-list/task.jsx
+++ src/components/task-list/task.jsx
@@ -1,7 +1,29 @@
-//import React from 'react'
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Checkbox from '@mui/material/Checkbox';
+import { useState } from "react";
+import { FaTrash } from "react-icons/fa6";
 
 export default function Task() {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
-    <div>task</div>
+    <Box sx={{bgcolor: '#D9D9D9', p: '10px', borderRadius: '10px', width: '90%', display: 'flex', justifyContent: 'space-between'}}>
+      <Typography>
+        Example task
+      </Typography>
+      <Box>
+        <Checkbox
+          checked={checked}
+          onChange={handleChange}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+        <FaTrash/>
+      </Box>
+    </Box>
   )
 }
