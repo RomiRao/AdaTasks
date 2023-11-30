da80047e0bd92ce664dfa422507e109d573b8cd9
RomiRao <raoromina96@gmail.com>
2023-11-29 23:18:39 -0300

    tasks container style done

--- src/components/task-list/tasksContainer.jsx
+++ src/components/task-list/tasksContainer.jsx
@@ -1,18 +1,20 @@
 //import React from 'react'
 
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Task from "./Task"
 import TasksInfo from "./TasksInfo"
+import image from "../../../public/images/chopper.png"
 
 export default function TasksContainer() {
   return (
-    <>
+    <Container sx={{bgcolor: '#FAF6F0', height: '100%', p: '20px', borderRadius: '10px', width: '90%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
       <Box>
         <Task />
       </Box>
-      <Box>
+      <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
         <TasksInfo />
+        <img src={image} alt="chopper illustration" style={{width: '50%'}}/>
       </Box>
-    </>
+    </Container>
   )
 }
