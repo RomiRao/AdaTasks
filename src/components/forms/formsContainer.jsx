537af4f14aaff5947d1f3c96eec41ea07dcb1c26
RomiRao <raoromina96@gmail.com>
2023-11-29 21:15:38 -0300

    okey, AT LEAST the structure is kinda done :D, ill fix the rest later on another branch, i need a break uwu

--- src/components/forms/formsContainer.jsx
+++ src/components/forms/formsContainer.jsx
@@ -8,7 +8,7 @@ import image from "../../../public/images/todo-title.png"
 export default function FormsContainer() {
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', p: '50px'}}>
-      <img style={{maxWidth: '450px', width: '90%'}} src={image} alt="title todo ilustration" />
+      <img style={{maxWidth: '450px', width: '90%'}} src={image} alt="title todo ilustration"/>
       <Submitform />
       <Showform />
     </Box>
