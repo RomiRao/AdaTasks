d67bc44e78f1d7604ae6d356b39ef3c7d6fe5d73
RomiRao <raoromina96@gmail.com>
2023-12-01 15:39:47 -0300

    givint setTasks property to task component

--- src/components/task-list/tasksContainer.jsx
+++ src/components/task-list/tasksContainer.jsx
@@ -5,12 +5,12 @@ import Task from "./Task"
 import TasksInfo from "./TasksInfo"
 import image from "../../assets/images/chopper.png"
 
-export default function TasksContainer({tasks}) {
+export default function TasksContainer({tasks, setTasks}) {
 
   return (
     <Container sx={{bgcolor: '#FAF6F0', boxShadow: 2 , height: '100%', p: '20px', pt: '30px', borderRadius: '10px', width: '90%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
       <Stack sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', overflowY: 'scroll', height: '67%'}} spacing={1}>
-        {tasks.map(task =>  (<Task task={task.task} category={task.category} complete={task.complete} />))}
+        {tasks.map(task =>  (<Task setTasks={setTasks} task={task.task} category={task.category} complete={task.complete} />))}
       </Stack>
       <Box sx={{display: 'flex', justifyContent: 'space-between', mt: '15px'}}>
         <TasksInfo />
