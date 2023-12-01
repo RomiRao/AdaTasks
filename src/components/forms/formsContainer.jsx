00890b20b63a3ce68642cb32228eddaa3225bbeb
RomiRao <raoromina96@gmail.com>
2023-12-01 15:27:05 -0300

    adding new property, tasks

--- src/components/forms/formsContainer.jsx
+++ src/components/forms/formsContainer.jsx
@@ -5,11 +5,11 @@ import Showform from "./Showform"
 import { Box } from "@mui/material";
 import image from "../../assets/images/todo-title.png"
 
-export default function FormsContainer() {
+export default function FormsContainer({setTasks, tasks}) {
   return (
     <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', p: '50px'}}>
       <img style={{maxWidth: '450px', width: '90%'}} src={image} alt="title todo ilustration"/>
-      <Submitform />
+      <Submitform setTasks={setTasks} tasks={tasks}/>
       <Showform />
     </Box>
   )
