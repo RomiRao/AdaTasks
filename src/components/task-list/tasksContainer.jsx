ae1566ec9caf6aa8ad81beea4615b1e08e4738c7
RomiRao <raoromina96@gmail.com>
2023-12-03 17:19:46 -0300

    height of tasks container changed and also giving tasks property to every task on the map function, i just try to not kill my self during this proces :D

--- src/components/task-list/tasksContainer.jsx
+++ src/components/task-list/tasksContainer.jsx
@@ -1,16 +1,16 @@
 //import React from 'react'
 
 import { Box, Container, Stack } from "@mui/material";
-import Task from "./Task"
+import Task from "./task"
 import TasksInfo from "./TasksInfo"
 import image from "../../assets/images/chopper.png"
 
 export default function TasksContainer({tasks, setTasks}) {
 
   return (
-    <Container sx={{bgcolor: '#FAF6F0', boxShadow: 2 , height: '100%', p: '20px', pt: '30px', borderRadius: '10px', width: '90%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+    <Container sx={{bgcolor: '#FAF6F0', boxShadow: 2 , height: '100%', maxHeight: '550px', p: '20px', pt: '30px', borderRadius: '10px', width: '90%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
       <Stack sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', overflowY: 'scroll', height: '67%'}} spacing={1}>
-        {tasks.map(task =>  (<Task setTasks={setTasks} task={task.task} category={task.category} complete={task.complete} />))}
+        {tasks.map(task =>  (<Task key={task.id} id={task.id} setTasks={setTasks} task={task.task} category={task.category} complete={task.complete} tasks={tasks} />))}
       </Stack>
       <Box sx={{display: 'flex', justifyContent: 'space-between', mt: '15px'}}>
         <TasksInfo />
