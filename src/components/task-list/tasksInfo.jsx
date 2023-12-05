dd2bb6fbc45478ea3da1ba0fde38c26811cace39
RomiRao <raoromina96@gmail.com>
2023-12-05 14:13:27 -0300

    making short the task info function

--- src/components/task-list/tasksInfo.jsx
+++ src/components/task-list/tasksInfo.jsx
@@ -5,31 +5,23 @@ import { MdOutlineCheckCircleOutline } from "react-icons/md";
 
 export default function TasksInfo({tasks}) {
 
-  const tasksDone = () => {
-    let tasksDone = 0;
-    tasks.forEach(task =>  task.complete === true ? tasksDone = tasksDone + 1 : tasksDone = tasksDone + 0)
-    return tasksDone
-  }
 
-  const tasksUnDone = () => {
-    let tasksUnDone = 0;
-    tasks.forEach(task =>  task.complete === false ? tasksUnDone = tasksUnDone + 1 : tasksUnDone = tasksUnDone + 0)
-    return tasksUnDone
+  const tasksInfo = (info) => {
+    let tasksInfo = 0;
+    tasks.forEach(task =>  task.complete === info ? tasksInfo = tasksInfo + 1 : tasksInfo = tasksInfo + 0)
+    return tasksInfo
   }
-
 
   return (
   <Box sx={{display: 'flex', alignItems: 'flex-end', width: '60%', mb: '10px'}}>
     <Box sx={{display: 'flex', width: '40%', alignItems: 'center' , justifyContent: 'space-around', bgcolor: '#FAD6D9', borderRadius: '3px', p: '5px'}}>
       <Box>
         <RxCrossCircled />
-        <Typography component='span' ml={1}>
-        {tasksUnDone()}
-        </Typography>
+        <Typography component='span' ml={1}>{tasksInfo(false)}</Typography>
       </Box>
       <Box>
         <MdOutlineCheckCircleOutline />
-        <Typography component='span' ml={1}>{tasksDone()}</Typography>
+        <Typography component='span' ml={1}>{tasksInfo(true)}</Typography>
       </Box>
     </Box>
   </Box>
