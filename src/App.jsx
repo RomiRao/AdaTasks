4d6bc3e3321d58f6c16ab1841347ad7b248c6571
RomiRao <raoromina96@gmail.com>
2023-12-05 15:23:41 -0300

    adjusting responsive

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <Container >
-      <Grid container pt={12}>
+      <Grid container pt={{ xs: 0, sm: 7, md: 10 }} spacing={2} justifyContent="center">
         <Grid item xs={12} md={6}>
           <FormsContainer setTasks={setTasks} setFilter={setFilter} tasks={tasks} filter={filter}/>
         </Grid>
