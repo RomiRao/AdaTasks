fee8252efc7f4d391d53e5aa6aec3f7b98470612
RomiRao <raoromina96@gmail.com>
2023-12-11 19:24:10 -0300

    changing checked for complete

--- src/components/task-list/task.jsx
+++ src/components/task-list/task.jsx
@@ -1,23 +1,19 @@
 import { Typography,  IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import Checkbox from '@mui/material/Checkbox';
-import { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { AiFillExclamationCircle } from "react-icons/ai";
 
 
 export default function Task({id, task, category, complete, setTasks, tasks}) {
-  const [checked, setChecked] = useState(complete);
 
   const handleChange = (event) => {
-    const newChecked = event.target.checked
-    setChecked(newChecked);
-      const updatedTasks = tasks.map((t) =>
-        t.id === id ? { ...t, complete: newChecked } : t
-      );
-      
-      localStorage.setItem("Tasks", JSON.stringify(updatedTasks));
-      setTasks(updatedTasks);
+    const updatedTasks = tasks.map((t) =>
+      t.id === id ? { ...t, complete: event.target.checked } : t
+    );
+
+    localStorage.setItem("Tasks", JSON.stringify(updatedTasks));
+    setTasks(updatedTasks);
   };
 
   const handleDelete = () => {
@@ -26,13 +22,14 @@ export default function Task({id, task, category, complete, setTasks, tasks}) {
     localStorage.setItem("Tasks", JSON.stringify(updatedTasks));
   };
 
+
   return (
     <Box sx={{bgcolor: '#d5e7f2', py: '7px', px: '20px', borderRadius: '60px', width: '90%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: { xs: 'column', sm: 'row', md: 'row' }}}>
       {complete? <Typography sx={{textDecoration: 'line-through', color: 'gray'}}>{task}</Typography> : <Typography color='#4E6AAC'>{task}</Typography> }
       <Box sx={{display: 'flex', alignItems: 'center'}}>
         {category && <AiFillExclamationCircle color='red' />}
         <Checkbox
-          checked={checked}
+          checked={complete}
           onChange={(e) => handleChange(e)}
           inputProps={{ 'aria-label': 'controlled' }}
         />
