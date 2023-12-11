648112dba7e0b9ef3575482bb6b514a09196a3d3
RomiRao <raoromina96@gmail.com>
2023-12-11 19:21:34 -0300

    reestructuring filter

--- src/components/task-list/tasksContainer.jsx
+++ src/components/task-list/tasksContainer.jsx
@@ -15,12 +15,11 @@ export default function TasksContainer({tasks, setTasks, filter}) {
     });
   }
   
-  const filteredTasks = filtered(filter);
 
   return (
     <Container sx={{bgcolor: 'white', boxShadow: 2 , height: '100%', maxHeight: '550px', p: '20px', pt: '30px', borderRadius: '10px', width: '90%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
       <Stack sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', overflowY: 'scroll', height: '67%'}} spacing={1}>
-      {filteredTasks.map((task) => (
+      {filtered(filter).map((task) => (
           <Task key={task.id} id={task.id} setTasks={setTasks} task={task.task} category={task.category} complete={task.complete} tasks={tasks} />
         ))}
       </Stack>
@@ -31,5 +30,3 @@ export default function TasksContainer({tasks, setTasks, filter}) {
     </Container>
   )
 }
-// tasks.map(task =>  (<Task key={task.id} id={task.id} setTasks={setTasks} task={task.task} category={task.category} complete={task.complete} tasks={tasks} />))
-
